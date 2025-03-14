2a5eca82ddac0fc519cf8629ec2baf9d22d39944
NextGenDataLead <dekker.i@gmail.com>
2025-03-14 20:52:16 +0100

    fixed link general inquiries

--- src/components/layout/Footer/ContactLinks.tsx
+++ src/components/layout/Footer/ContactLinks.tsx
@@ -5,15 +5,8 @@ const ContactLinks = () => (
     <h3 className="text-lg font-semibold text-white mb-2">Contact</h3>
     <a href="https://dappastra.notion.site/16cb2ca320a780c0903ae295795b6e5f?pvs=105" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">Need Developer?</a>
     <a href="https://dappastra.com/developer-network" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">Join as Developer</a>
-    <a 
-      href="mailto:no-reply@dappastra.com?subject=General%20Inquiry" 
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-gray-300 hover:text-white transition-colors"
-    >
-      General Inquiries
-    </a>
-  </div>
+    <a href="https://dappastra.notion.site/176b2ca320a78049a91aeb91559bc48f" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition-colors">General Inquiries</a>
+   </div>
 );
 
 export default ContactLinks;
